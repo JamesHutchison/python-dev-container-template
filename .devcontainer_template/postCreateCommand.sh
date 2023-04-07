d65672c2c158b4844ce2f20870eb38daea179361
James Hutchison <122519877+JamesHutchison@users.noreply.github.com>
2023-04-07 00:07:46 +0000

    consolidate to single command

--- .devcontainer_template/postCreateCommand.sh
+++ .devcontainer_template/postCreateCommand.sh
@@ -7,8 +7,9 @@ sudo chown vscode .venv || true
 # make the python binary location predictable
 poetry config virtualenvs.in-project true
 poetry init -n --python=~3.11 || true
-poetry add mypy --group=dev || true  # static type checking
-poetry add ruff --group=dev || true  # fast flake8 and isort alternative
-poetry add black --group=dev || true  # opinionated formatting
-poetry add pytest --group=dev || true  # testing
+# mypy - static type checking
+# ruff - fast flake8 and isort alternative
+# black - opinionated formatting
+# pytest - testing
+poetry add mypy ruff black pytest --group=dev || true
 poetry install --with=dev || true
