4074a8c0a4a95f6fbab087e9ec62f7fd17d02a60
James Hutchison <122519877+JamesHutchison@users.noreply.github.com>
2023-05-24 15:04:13 -0700

    ensure nohup has a chance to run

--- .devcontainer_template/postStartCommand.sh
+++ .devcontainer_template/postStartCommand.sh
@@ -2,3 +2,6 @@
 
 # run in the background at startup
 nohup bash .devcontainer/postStartBackground.sh > ".dev_container_logs/postStartBackground.out" &
+
+# ensure nohup has a chance to run
+sleep 3
