bc4064fcedb30298c1fe13a78e46556f8c779df1
James Hutchison <122519877+JamesHutchison@users.noreply.github.com>
2023-05-31 19:19:34 -0700

    create .dev_container_logs

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+mkdir -p .dev_container_logs
+echo "*" > .dev_container_logs/.gitignore
+
 # run in the background at startup
 nohup bash .devcontainer/postStartBackground.sh > ".dev_container_logs/postStartBackground.out" &
