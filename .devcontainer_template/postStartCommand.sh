5040fd7f8a66e20886c118b505e44ee13480168a
James Hutchison <122519877+JamesHutchison@users.noreply.github.com>
2023-05-31 19:17:56 -0700

    create .dev_container_logs if it doesnt exist

--- .devcontainer_template/postStartCommand.sh
+++ .devcontainer_template/postStartCommand.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+mkdir -p .dev_container_logs
+
 # run in the background at startup
 nohup bash .devcontainer/postStartBackground.sh > ".dev_container_logs/postStartBackground.out" &
 
